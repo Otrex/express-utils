e668102bc954b54184d62d150daa12dbe200e0b0
Treasure <obisiket@yahoo.com>
2023-03-23 00:05:11 +0100

    feat: added middlewares; auth and general

--- test/test.ts
+++ test/test.ts
@@ -1,31 +1,31 @@
-import { useDecorator } from './../src/index';
-import express from 'express';
-// Async Handler no =t yet working
-const app = express();
+// import { useDecorator } from './../src/index';
+// import express from 'express';
+// // Async Handler no =t yet working
+// const app = express();
 
-const { Controller, globalMiddleware, asyncHandler } = useDecorator();
+// const { Controller, globalMiddleware, asyncHandler } = useDecorator();
 
-@globalMiddleware('/ok')
-class BaseController extends Controller {
-  @BaseController.addRoute({ method: 'get', path: '/omit', useAsyncHandler: true, validator: (req: any) => { } })
-  async hello (req: any, res: any, next: any) {
-    console.log(req); 
-    res.send('Ok')
-  }
+// @globalMiddleware('/ok')
+// class BaseController extends Controller {
+//   @BaseController.addRoute({ method: 'get', path: '/omit', useAsyncHandler: true, validator: (req: any) => { } })
+//   async hello (req: any, res: any, next: any) {
+//     console.log(req); 
+//     res.send('Ok')
+//   }
 
-  @BaseController.addRoute({ method: 'get' })
-  helloMe(req: any, res: any, next: any) {
-    res.send('OkMe')
-  }
-}
+//   @BaseController.addRoute({ method: 'get' })
+//   helloMe(req: any, res: any, next: any) {
+//     res.send('OkMe')
+//   }
+// }
 
-app.get('/kk', asyncHandler((req: any, res: any, next: any) => {
-  throw new Error('hello Error')
-}))
+// app.get('/kk', asyncHandler((req: any, res: any, next: any) => {
+//   throw new Error('hello Error')
+// }))
 
-app.use((err: any, req: any, res: any, next:any) => {
-  res.send(err.message);
-})
-app.use(BaseController.registerRoutes(express.Router()))
+// app.use((err: any, req: any, res: any, next:any) => {
+//   res.send(err.message);
+// })
+// app.use(BaseController.registerRoutes(express.Router()))
 
-app.listen(8011, () => console.log("Working"))
+// app.listen(8011, () => console.log("Working"))
